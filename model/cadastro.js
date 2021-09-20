4bf025529cf2ff9beb874c201f4fb88bb8099f51
Jose Ribamar Junior <jribamarjunior89@gmail.com>
2021-09-19 21:22:33 -0300

    ajuste dos arquivos para funcionamento

--- model/cadastro.js
+++ model/cadastro.js
@@ -1,24 +1,19 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const Cadastro = new Schema({
- status_lampada: {
-  type: Boolean, 
-  required: [true, 'Status Obrigatório'], 
-  max: 20
- },
- tipo_lampada: {
+const CadastroSchema = new Schema({
+ nome_lampada: {
   type: String, 
-  required: [true, 'Tipo Obrigatório'], 
+  required: [true, 'Nome Obrigatório'], 
   max: 100
  },
- nome_lampada: {
-    type: String, 
-    required: [true, 'Nome Obrigatório'], 
-    max: 100
-},
- voltagem: {
+ voltagem_lampada: {
   type: String, 
-  required: [true, 'Voltagem Obrigatória']}
+  required: [true, 'E-mail Obrigatório'], 
+  max: 100
+ },
+ status_lampada: {
+  type: String, 
+  required: [true, 'CPF Obrigatório']}
  });
 // Exportar o modelo
-module.exports = mongoose.model('Cadastro', Cadastro);
+module.exports = mongoose.model('cadastro', CadastroSchema);

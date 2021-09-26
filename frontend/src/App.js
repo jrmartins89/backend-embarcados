8f98bf25940e74153f2e88b5d9332ffc17ef4993
Jose Ribamar Junior <jribamarjunior89@gmail.com>
2021-09-25 21:03:56 -0300

    instalação do reactive native e criação do frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Lampadas from "./Lampada";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Lampadas />
+      </header>
+    </div>
+  );
+}export default App;
---
+++ frontend/src/Lampada.jsx
@@ -0,0 +1,11 @@
+import React, {useState} from "react";
+
+function Lampadas() {
+    return (
+      <div>
+        <button>
+          Mostrar Lampadas
+        </button>
+      </div>
+    )
+  }export default Lampadas;
